003cb4d32f34ce9b5ed5263fab6fc55b5d4cb774
Roman <roman1romanyshyn@gmail.com>
2025-05-02 19:25:01 +0300

    fix logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata = {
   title: "Roman Romanyshyn - Senior Full Stack Developer Portfolio",
   description: "Personal website portfolio Senior Full Stack developer.",
   icons: {
-    icon: [{ url: "/favicon_rocket.ico", type: "image/x-icon" }],
+    icon: [{ url: "/logo.png", type: "image/x-icon" }],
   },
 };
 
